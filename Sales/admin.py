bfc848996e0d3103ea4608076f4bc42ff7ad3fce
Bhola Maru <bholamarubhola@gmail.com>
2025-07-03 12:19:01 +0530

    Update admin.py
    add all

--- Sales/admin.py
+++ Sales/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import onwardchallan, ChallanCounter
+from .models import transportdetails
+from .models import vehicaldetails
+from .models import outwardchallan
+
+@admin.register(onwardchallan)
+class OnwardChallanAdmin(admin.ModelAdmin):
+    readonly_fields = ('challan_no',)
+    # or to hide it completely:
+    # exclude = ('challan_no',)
+@admin.register(transportdetails)
+class TransportDetailsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(vehicaldetails)
+class VehicalDetailsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ChallanCounter)
+class ChallanCounterAdmin(admin.ModelAdmin):
+    pass
+@admin.register(outwardchallan)
+class outwardchallanAdmin(admin.ModelAdmin):
+    pass
